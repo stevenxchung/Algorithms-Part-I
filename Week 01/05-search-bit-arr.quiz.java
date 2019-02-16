f8883a7a97a37173a3b376344ac8b5538827f0f2
Steven <stevenxchung@gmail.com>
2019-02-16 09:07:06 -0500

    Create 05-search-bit-arr.quiz.java

---
+++ Week 01/05-search-bit-arr.quiz.java
@@ -0,0 +1,24 @@
+public class FindBit {
+
+	boolean findBit(int nums[], int size, int key) {
+		// First
+    for (int k = 0; k < size; k++) {
+      if (nums[k] == key) {
+        System.out.print("Key: " + nums[k]);
+        return true;
+      }
+    }
+		// No key was found
+		return false;
+	}
+
+	// Test function
+	public static void main(String[] args) {
+		FindBit bit = new FindBit();
+		int nums[] = { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
+		int key = 5;
+		int size = nums.length;
+
+		bit.findBit(nums, size, key);
+	}
+}
