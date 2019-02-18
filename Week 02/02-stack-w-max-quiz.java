e923d34bec45a05193bd2c97556ca36d02ae4ab4
Steven <stevenxchung@gmail.com>
2019-02-18 18:31:49 -0500

    Add stack with max quiz and testing

---
+++ Week 02/02-stack-w-max-quiz.java
@@ -0,0 +1,47 @@
+// Stack with max
+import java.util.Stack;
+import java.util.TreeSet;
+
+public class StackWithMax<Item> extends Stack<Item> {
+
+  private TreeSet<Item> tree = new TreeSet<Item>();
+
+  // TreeSet.last() returns the last item which is the max
+  public Item max() {
+    return tree.last();
+  }
+
+  // Insert an element
+  public Item push(Item item) {
+    // Use push() from the parent class Stack
+    super.push(item);
+    tree.add(item);
+    return item;
+  }
+
+  // Pop off an element
+  public Item pop() {
+    Item item = super.pop();
+    tree.remove(item);
+    return item;
+  }
+
+  // Testing
+  public static void main(String[] args) {
+    StackWithMax<Integer> maxStack = new StackWithMax<Integer>();
+    int a = 1;
+    int b = 20;
+    int c = 100;
+
+    maxStack.push(a);
+    maxStack.push(b);
+    maxStack.push(c);
+
+    System.out.println("Max: " + maxStack.max());
+
+    maxStack.pop();
+    maxStack.pop();
+
+    System.out.println("Max: " + maxStack.max());
+  }
+}
