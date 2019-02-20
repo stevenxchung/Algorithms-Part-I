bcbd68c2c36a2ec6fd893106a0bc6d9ca487688c
Steven <stevenxchung@gmail.com>
2019-02-20 18:35:14 -0500

    Update Permutation.java

--- Week 02/queues/tests/Permutation.java
+++ Week 02/queues/tests/Permutation.java
@@ -1,4 +1,18 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 // Takes an integer k, reads in a sequence of strings and prints exactly k of them
 public class Permutation {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        RandomizedQueue<String> strings = new RandomizedQueue<String>();
+        while (!StdIn.isEmpty()) {
+            strings.enqueue(StdIn.readString());
+        }
+
+        // Use k as number of args
+        int k = Integer.parseInt(args[0]);
+        for (int i = 0; i < k; i++) {
+            StdOut.println(strings.dequeue());
+        }
+    }
 }
