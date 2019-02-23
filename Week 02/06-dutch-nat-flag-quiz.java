ff619891947a775af6e2b82c49db161e9a290144
Steven <stevenxchung@gmail.com>
2019-02-22 19:23:20 -0500

    Create 06-dutch-nat-flag-quiz.java

---
+++ Week 02/06-dutch-nat-flag-quiz.java
@@ -0,0 +1,31 @@
+// Dutch national flag
+
+// Simplest case in pseudo-code
+public class DutchNationalFlag {
+
+  public void Swap(int[][] a, int bucketA, int bucketB) {
+    System.out.println("Array before swap: " + a);
+    String temp = "";
+    temp = a[bucketA][0];
+    a[bucketA][0] = a[bucketB][0];
+    a[bucketB][0] = temp;
+    System.out.println("Array after swap: " + a);
+  }
+
+  public void Color(int[][] a, int bucket) {
+    System.out.println("Pebble color: " + a[bucket][0]);
+  }
+
+  public static void main(String[] args) {
+    DutchNationalFlag obj = new DutchNationalFlag();
+    int[][] a = new int[][] {
+      {"red"},
+      {"white"},
+      {"blue"}
+      // {n} ...
+    };
+
+    obj.Swap(a, 0, 1);
+    obj.Color(a, 0);
+  }
+}
