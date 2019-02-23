8006d247cf656cf02eb6406065f7e014c0d1d932
Steven <stevenxchung@gmail.com>
2019-02-22 19:23:14 -0500

    Create 04-intersect-two-sets-quiz.java

---
+++ Week 02/04-intersect-two-sets-quiz.java
@@ -0,0 +1,33 @@
+// Intersection of two sets
+
+// Simplest case in pseudo-code
+public class Intersection {
+  public int countMatches(int[][] a, int[][] b) {
+    int count = 0;
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[i].length; j++) {
+        if (a[i][j] == b[i][j]) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    Intersection obj = new Intersection();
+    int[][] a = new int[][] {
+      {1, 2},
+      {3, 4},
+      {5, 6},
+      {7, 8},
+    };
+    int[][] b = new int[][] {
+      {1, 2},
+      {3, 4},
+      {5, 6},
+      {7, 8},
+    };
+    System.out.println("There are " + obj.countMatches(a, b) + " matches found!");
+  }
+}
