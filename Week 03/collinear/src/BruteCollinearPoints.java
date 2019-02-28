d26104eea2f07300babf70e6c355a5746c9102bd
Steven <stevenxchung@gmail.com>
2019-02-27 21:45:27 -0500

    Source files initial commit

--- Week 03/collinear/src/BruteCollinearPoints.java
+++ Week 03/collinear/src/BruteCollinearPoints.java
@@ -1,2 +1,14 @@
 public class BruteCollinearPoints {
+
+    // Finds all line segments containing four points
+    public BruteCollinearPoints(Point[] points) {
+    }
+
+    // The number of line segments
+    public int numberOfSegments() {
+    }
+
+    // The line segments
+    public LineSegment[] segments() {
+    }
 }
--- Week 03/collinear/src/FastCollinearPoints.java
+++ Week 03/collinear/src/FastCollinearPoints.java
@@ -1,2 +1,15 @@
 public class FastCollinearPoints {
+
+    // Finds all line segments containing four or more points
+    public FastCollinearPoints(Point[] points) {
+    }
+
+    // The number of line segments
+    public int numberOfSegments() {
+    }
+
+    // The line segments
+    public LineSegment[] segments() {
+    }
+
 }
