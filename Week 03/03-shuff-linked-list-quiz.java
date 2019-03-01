7032e210af61eea20ffa220401bb4a7f987385da
Steven <stevenxchung@gmail.com>
2019-03-01 16:26:18 -0500

    Create 03-shuff-linked-list-quiz.java

---
+++ Week 03/03-shuff-linked-list-quiz.java
@@ -0,0 +1,31 @@
+// Shuffling a linked list
+// Pseudo-code simplest case
+public class ShufflingLinkedList {
+
+  // Can shuffle a deck by cutting deck in half and alternate cards during merge
+  public void shufflingLinkedList(Node head) {
+    // Only move on if deck has cards
+    if (head == NULL) {
+      return;
+    }
+
+    // Spits deck at halfway point
+    Node half = splitAtHalfway(head);
+    // Alternate cards when merging
+    alternateMerge(head, half);
+    // Head of the list will be the card at the halfway point
+    head = half;
+  }
+
+  public void splitAtHalfway(Node first) {
+    // Spit deck at halfway point
+  }
+
+  public void alternateMerge(Node first, Node second) {
+    // Merge the to halves by alternating cards
+  }
+
+  public static void main(String[] args) {
+    // Test
+  }
+}
