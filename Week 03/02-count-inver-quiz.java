230f525c4adec4388b35a10c49a307fcff65363a
Steven <stevenxchung@gmail.com>
2019-03-01 16:26:15 -0500

    Create 02-count-inver-quiz.java

---
+++ Week 03/02-count-inver-quiz.java
@@ -0,0 +1,25 @@
+// Counting inversions
+public class CountingInversions {
+
+  public int countingInversions(int[] arr, int n) {
+    // Initalize count to zero
+    int count = 0;
+    // Loop over array for the entire length
+    for (int i = 0; i < n - 1; i++) {
+      // For each element check if arr[i] > arr[j]
+      for (int j = i + 1; j < n; j++) {
+        if (arr[i] > arr[j]) {
+          count++;
+        }
+      }
+    }
+
+    return count;
+  }
+
+  public static void main(String[] args) {
+    CountingInversions obj = new CountingInversions();
+    int[] testArr = new int[] { 1, 2, 1, 2, 3 };
+    System.out.println("There are " + obj.countingInversions(testArr, testArr.length) + " inversions!");
+  }
+}
