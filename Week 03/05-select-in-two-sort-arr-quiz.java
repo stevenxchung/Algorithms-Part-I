a3d842415f8beb374e275afb257b01c36487d9fe
Steven <stevenxchung@gmail.com>
2019-03-03 16:39:51 -0500

    Create 05-select-in-two-sort-arr-quiz.java

---
+++ Week 03/05-select-in-two-sort-arr-quiz.java
@@ -0,0 +1,22 @@
+// Selection in two sorted arrays
+// Pseudo-code simplest case where array sizes are equal, values are equal
+public class SelectionInTwoSortedArrays {
+  // Compares two arrays and returns the largest key (largest match)
+  private static int compare(int[] arrA, int[] arrB) {
+    int largest = 0;
+    int lenA = arrA.length;
+    int lenB = arrB.length;
+    // If sorted, largest value is always the last one...
+    if (arrA[lenA - 1] != arrB[lenB - 1]) {
+      // Arrays not valid
+      return -1;
+    } else {
+      // arrB[lenB - 1] works fine
+      return largest = arrA[lenA - 1];
+    }
+  }
+
+  public static void main(String[] args) {
+    // Test
+  }
+}
