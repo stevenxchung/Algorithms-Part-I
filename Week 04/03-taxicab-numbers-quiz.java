5c1dbec7eb9b12e5d8a311a5147e4062e1c14073
Steven <stevenxchung@gmail.com>
2019-03-05 20:38:34 -0500

    Create 03-taxicab-numbers-quiz.java

---
+++ Week 04/03-taxicab-numbers-quiz.java
@@ -0,0 +1,36 @@
+// Taxicab numbers
+import java.util.*;
+
+public class TaxicabNumbers {
+
+  public int taxicabNumbers(int n) {
+    int taxiNum = 1;
+    int firstCount = 0;
+
+    // Until firstCount == requested nth taxicab number
+    while (firstCount < n) {
+      // Reset to 0 each loop
+      int secondCount = 0;
+
+      // Try all pairs for a^3 + b^3 = taxNum
+      for (int a = 1; a <= Math.pow(taxiNum, 1/3); a++) {
+        for (int b = a + 1; b <= Math.pow(taxiNum, 1/3); b++) {
+          if (Math.pow(a, 3) + Math.pow(b, 3) == taxiNum) {
+            secondCount++;
+          }
+        }
+      }
+
+      taxiNum++;
+    }
+
+    return taxiNum;
+  }
+
+  public static void main(String[] args) {
+    // Test
+    int n = 5;
+    TaxicabNumbers obj = new TaxicabNumbers();
+    System.out.println("Iteration no. " + n + " has a taxicab number of " + obj.taxicabNumbers(5));
+  }
+}
