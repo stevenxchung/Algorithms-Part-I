36de0b26e748ef017fd976a19831b4c0bb5affd1
Steven <stevenxchung@gmail.com>
2019-03-05 20:55:27 -0500

    Create Board.java

---
+++ Week 04/8puzzle/src/Board.java
@@ -0,0 +1,52 @@
+public class Board {
+    // Construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] blocks) {
+
+    }
+
+    // Board dimension n
+    public int dimension() {
+
+    }
+
+    // Number of blocks out of place
+    public int hamming() {
+
+    }
+
+    // Sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+
+    }
+
+    // Is this board the goal board?
+    public boolean isGoal() {
+
+    }
+
+    // A board that is obtained by exchanging any pair of blocks
+    public Board twin() {
+
+    }
+
+    // Does this board equal y?
+    public boolean equals(Object y) {
+
+    }
+
+    // All neighboring boards
+    public Iterable<Board> neighbors() {
+
+    }
+
+    // String representation of this board (in the output format specified below)
+    public String toString() {
+
+    }
+
+    // Unit tests (not graded)
+    public static void main(String[] args) {
+
+    }
+}
