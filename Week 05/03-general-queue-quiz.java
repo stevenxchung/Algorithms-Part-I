3ec3fd224ae51d2a6b96dc12e8c21b4759be0519
Steven <stevenxchung@gmail.com>
2019-03-12 18:35:24 -0400

    Create 03-general-queue-quiz.java

---
+++ Week 05/03-general-queue-quiz.java
@@ -0,0 +1,30 @@
+// Generalized queue
+// Pseudo-code
+
+public class GeneralizedQueue {
+  private index;
+  private BST<>();
+
+  // Create an empty data structure
+  GeneralizedQueue() {
+    index = 0;
+    tree = new BST<>();
+  }
+
+  // Append an item to the end of the queue
+  public void append() {
+  }
+
+  // Remove an item from the front of the queue
+  public void remove() {
+  }
+
+  // Return the ith item in the queue
+  public void ithItem() {
+  }
+
+  // Remove the ith item from the queue
+  public void removeIthItem() {
+    remove(ithItem(/* input */))
+  }
+}
