2a148878598264d0b68c4fd7c7e84dd190b68532
Steven <stevenxchung@gmail.com>
2019-03-13 19:05:35 -0400

    Create KdTree.java

---
+++ Week 05/kdtree/src/KdTree.java
@@ -0,0 +1,25 @@
+public class KdTree {
+    // Construct an empty set of points
+    public KdTree() {}
+
+    // Is the set empty?
+    public boolean isEmpty() {}
+
+    // Add the point to the set (if it is not already in the set)
+    public void insert(Point 2D p) {}
+
+    // Does the set contain point p?
+    public boolean contains(Point2D p) {}
+
+    // Draw all points to standard draw
+    public void draw() {}
+
+    // All points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {}
+
+    // A nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {}
+
+    // Unit testing of the methods (optional)
+    public static void main(String[] args) {}
+}
